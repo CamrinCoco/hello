124f986478695545d19502ca67963ff4e050ead0
CamrinCoco <142856932+CamrinCoco@users.noreply.github.com>
2023-08-24 09:47:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+Author: Camrin Coco and Jackson Wiese
+Date: 2023/08/24
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Camrin and Jack\n");
+
+}
