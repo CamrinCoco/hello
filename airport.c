b20ffbb156e187f741d44735cb9602ab52d6a01e
CamrinCoco <142856932+CamrinCoco@users.noreply.github.com>
2023-11-30 10:42:30 -0600

    Add files via upload

--- airport.c
+++ airport.c
@@ -1,26 +1,9 @@
-/* Authors: Camrin Coco and Jackson Wiese
-
-Purpose: To create and establish structures for for use with the airport structure.
-Also to create airpot fucntions using the structure.
-
-Date: 11/10/2023
-
-*/
-
-
-
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
 #include <math.h>
+#include <string.h>
 
 #include "airport.h"
-
-Airport * createEmptyAirport() {
-    return createAirport("", "", "", 0, 0, 0, "", "");
-}
 
 Airport* createAirport(const char* gpsId,const char* type, const char* name,
 double latitude,double longitude, int elevationFeet,const char* city, const char* countryAbbrv) {
@@ -79,25 +62,25 @@ double latitude, double longitude, int elevationFeet, const char* city, const ch
     return;
 }
 
-char* airportToString(const Airport* a) {
-    if (a == NULL) {
-        char *result = (char *)malloc(sizeof(char) * 7);
-        strcpy(result, "(null)");
-        return result;
-    }
-    char buffer[100];
-    // compute the number of characters needed
-    //int n = strlen(a->gpsId) + strlen(a->type) + strlen(a->name) + 10 + 10 + 10 + strlen(a->city) + strlen(a->countryAbbrv);
+// char* airportToString(const Airport* a) {
+//     if (a == NULL) {
+//         char *result = (char *)malloc(sizeof(char) * 7);
+//         strcpy(result, "(null)");
+//         return result;
+//     }
+//     char buffer[100];
+//     // compute the number of characters needed
+//     //int n = strlen(a->gpsId) + strlen(a->type) + strlen(a->name) + 10 + 10 + 10 + strlen(a->city) + strlen(a->countryAbbrv);
 
-    int n = snprintf(buffer, 100, "%s %s %s %lf %lf %d %s %s", a->gpsId, a->type, a->name, a->latitude, a->longitude, a->elevationFeet, a->city, a->countryAbbrv);
-    // create a result string
-    char *str = (char *)malloc(sizeof(char) * n);
+//     int n = snprintf(buffer, 100, "%s %s %s %lf %lf %d %s %s", a->gpsId, a->type, a->name, a->latitude, a->longitude, a->elevationFeet, a->city, a->countryAbbrv);
+//     // create a result string
+//     char *str = (char *)malloc(sizeof(char) * n);
 
-    // format the aiport into the temporary string
-    sprintf(str, "%s %s %s %.2lf %.2lf %d %s %s", a->gpsId, a->type, a->name, a->latitude, a->longitude, a->elevationFeet, a->city, a->countryAbbrv);
+//     // format the aiport into the temporary string
+//     sprintf(str, "%s %s %s %.2lf %.2lf %d %s %s", a->gpsId, a->type, a->name, a->latitude, a->longitude, a->elevationFeet, a->city, a->countryAbbrv);
 
-    return str;
-}
+//     return str;
+// }
 
 double getAirDistance(const Airport* origin, const Airport* destination) {
 
@@ -120,3 +103,256 @@ double getEstimatedTravelTime(const Airport* stops, int size, double aveKmsPerHo
 
     return travelTime;
 }
+
+void generateReports(Airport *airports, int n) {
+
+  printf("Airports (original): \n");
+  printf("==============================\n");
+  printAirports(airports, n);
+
+  printf("\nAirports By GPS ID: \n");
+  printf("==============================\n");
+
+  int i, j;
+  Airport temp;
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByGPSId(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Type: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByType(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Name: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByName(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Name - Reversed: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByNameDesc(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Country/City: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByCountryCity(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Latitude: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByLatitude(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Longitude: \n");
+  printf("==============================\n");
+
+  for(i = 0; i < n - 1; i++){
+    for(j = i + 1; j < n; j++){
+      if (cmpByLongitude(&airports[j], &airports[i]) > 0){
+        temp = airports[i];
+        airports[i] = airports[j];
+        airports[j] = temp;
+      }
+    }
+  }
+  printAirports(airports, n);
+
+  printf("\nAirports By Distance from Lincoln: \n");
+  printf("==============================\n");
+
+
+
+  printf("\nClosest Airport to Lincoln: \n");
+  printf("==============================\n");
+
+  printf("\nFurthest Airport from Lincoln: \n");
+  printf("==============================\n");
+
+  printf("\nEast-West Geographic Center: \n");
+  printf("==============================\n");
+
+  printf("\nNew York, NY airport: \n");
+  printf("==============================\n");
+  //if none found, print: "No New York airport found!\n"
+
+  printf("\nLarge airport: \n");
+  printf("==============================\n");
+  //if none found, print: "No large airport found!\n"
+
+
+  return;
+}
+
+char* airportToString(const Airport* a) {
+  char temp[1000];
+  //this formatting is required but the code may need to be adapted
+  // to your definiion of your Airport structure.
+  sprintf(temp, "%-8s %-15s %-20s %.2f %.2f %d %-10s %-2s", a->gpsId, a->type, a->name,
+          a->latitude, a->longitude, a->elevationFeet, a->city,
+          a->countryAbbrv);
+  char* result = (char*)malloc(sizeof(char) * (strlen(temp) + 1));
+  strcpy(result, temp);
+  return result;
+}
+
+void printAirports(Airport *airports, int n) {
+
+  for(int i=0; i<n; i++) {
+    char *s = airportToString(&airports[i]);
+    printf("%s\n", s);
+    free(s);
+  }
+
+  return;
+}
+
+
+int cmpByGPSId(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+
+  if(t1 ->gpsId > t2 ->gpsId){
+    return 1;
+  }
+  else if(t1 ->gpsId == t2 ->gpsId){
+    return 0;
+  }
+  else{
+    return -1;
+  }
+}
+
+int cmpByType(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+  return strcmp(t1->type, t2->type);
+}
+
+int cmpByName(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+  return strcmp(t1->name, t2->name);
+}
+
+int cmpByNameDesc(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+  return strcmp(t1->name, t2->name);
+}
+
+int cmpByCountryCity(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+  if (strcmp(t1->countryAbbrv, t2->countryAbbrv) == 0) {
+    return strcmp(t1->city, t2->city);
+  } else {
+    return strcmp(t1->countryAbbrv, t2->countryAbbrv);
+  }
+}
+
+int cmpByLatitude(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+
+  if(t1 ->latitude > t2 ->latitude){
+    return 1;
+  }
+  else if(t1 ->latitude == t2 ->latitude){
+    return 0;
+  }
+  else{
+    return -1;
+  }
+}
+
+int cmpByLongitude(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+
+  if(t1 ->longitude > t2 ->longitude){
+    return 1;
+  }
+  else if(t1 ->longitude == t2 ->longitude){
+    return 0;
+  }
+  else{
+    return -1;
+  }
+}
+
+int cmpByLincolnDistance(const void* a, const void* b){
+
+  const Airport *t1 = (const Airport *)a;
+  const Airport *t2 = (const Airport *)b;
+
+  if(t1 ->distance > t2 ->distance){
+    return 1;
+  }
+  else if(t1 ->distance == t2 ->distance){
+    return 0;
+  }
+  else{
+    return -1;
+  }
+}
+
--- airport.h
+++ airport.h
@@ -31,9 +31,7 @@ Airport* createAirport(const char* gpsId,
 
 /**
  * This function initializes an existing allocated
- * Airport structure with the given attributes. This
- * function should make *deep* copies of each of the
- * relevant fields.
+ * Airport structure with the given attributes.
  */
 void initAirport(Airport* airport,
                  const char* gpsId,
@@ -52,23 +50,84 @@ void initAirport(Airport* airport,
 char* airportToString(const Airport* a);
 
 /**
- * Computes the air distance, in kilometers, between
+ * Prints all the airports in the given array of n
+ * Airports.
+ */
+void printAirports(Airport *airports, int n);
+
+/**
+ * cmputes the air distance, in kilometers, between
  * the two Airports using their latitude/longitude
  */
 double getAirDistance(const Airport* origin, const Airport* destination);
 
 /**
- * Computes the estimated travel time (in hours) for a flight
- * that involves the given stops (an array of Airport structures
- * of the given size) using the average flight speed
- * (kilometers per hour).  Thus there are size - 1 "legs" of the
- * trip.
- *
- * The first stop is the origin and the last stop is the destination.
- * Each stop in between is a layover each incurring a wait time
- * of aveLayoverTimeHrs
+ * cmputes the estimated travel time (in hours) for a flight
+ * that involves the given stops using the average flight speed
+ * (kilometers per hour) and average layover time (in hours)
  */
 double getEstimatedTravelTime(const Airport* stops,
                               int size,
                               double aveKmsPerHour,
                               double aveLayoverTimeHrs);
+
+/**
+ * A comparator function that orders the two Airport structures by
+ * their GPS IDs in lexicographic order.
+ */
+int cmpByGPSId(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the two Airport structures by
+ * their type.
+ */
+int cmpByType(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the two Airport structures by
+ * their name in lexicographic order.
+ */
+int cmpByName(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the two Airport structures by
+ * their name in reverse lexicographic order.
+ */
+int cmpByNameDesc(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the two Airport structures first by
+ * country, then by city
+ */
+int cmpByCountryCity(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the given Airport structures
+ * by their latitudes north to south
+ */
+int cmpByLatitude(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the given Airport structures
+ * by their longitudes west to east
+ */
+int cmpByLongitude(const void* a, const void* b);
+
+/**
+ * A comparator function that orders the two Airport structures by
+ * their relative distance from Lincoln Municipal Airport
+ * (0R2, 40.846176, -96.75471)
+ * in ascending order according (closest to Lincoln would come first)
+ */
+int cmpByLincolnDistance(const void* a, const void* b);
+
+/**
+ * A function that generates and prints several reports on the
+ * given array of Airport structures.
+ */
+void generateReports(Airport *airports, int n);
+
+/**
+ * Prints an entire array of airports
+ */
+void printAirports(Airport *airports, int n);
